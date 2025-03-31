776046a38e2fcc356c6957a749e3c6d8a1836f22
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-03-31 16:31:53 -0700

    fix linting

---
+++ Blockchain/chaincodes/chaincode-kv-node/eslint.config.mjs
@@ -0,0 +1,94 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	js.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	prettier,
+	eslintPluginUnicorn.configs.recommended,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+			globals: {
+				...globals.browser,
+				...globals.node,
+				google: 'readonly',
+				gtag: 'readonly',
+			},
+		},
+		rules: {
+			// Ensures that correct values are returned, which is often a problem when generating directions.
+			'@typescript-eslint/explicit-function-return-type': [
+				'error',
+				{
+					allowExpressions: true,
+				},
+			],
+			// Makes Svelte snippets unusable
+			'@typescript-eslint/no-confusing-void-expression': 'off',
+			// conflicts with Svelte $props rune
+			'@typescript-eslint/no-unsafe-assignment': 'off',
+			'@typescript-eslint/no-unsafe-call': 'off',
+			'@typescript-eslint/only-throw-error': 'off',
+			'@typescript-eslint/prefer-nullish-coalescing': 'off',
+			'@typescript-eslint/prefer-promise-reject-errors': 'off',
+			'@typescript-eslint/restrict-template-expressions': [
+				'error',
+				{
+					allowAny: false,
+					allowBoolean: true,
+					allowNullish: false,
+					allowNumber: true,
+					allowRegExp: false,
+					allowNever: false,
+				},
+			],
+			'@typescript-eslint/unbound-method': 'off',
+			'unicorn/no-array-reduce': 'off',
+			'unicorn/no-await-expression-member': 'off',
+			// prettier edits the fix away
+			'unicorn/no-nested-ternary': 'off',
+			'unicorn/no-null': 'off',
+			'unicorn/filename-case': 'off',
+			'unicorn/prefer-spread': 'off',
+			'unicorn/prevent-abbreviations': [
+				'error',
+				{
+					allowList: {
+						$$Props: true,
+						// arguments is often an illegal variable name
+						args: true,
+						dev: true,
+						i: true,
+						param: true,
+						prod: true,
+						Props: true,
+						props: true,
+						Ref: true,
+						ref: true,
+						src: true,
+						utils: true,
+					},
+				},
+			],
+			curly: ['error', 'multi-line'],
+			eqeqeq: 'error',
+			'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+			yoda: 'error',
+		},
+		linterOptions: {
+			reportUnusedDisableDirectives: 'error',
+		},
+	},
+
+	{
+		ignores: ['dist'],
+	},
+);
--- Blockchain/chaincodes/chaincode-kv-node/src/index.ts
+++ Blockchain/chaincodes/chaincode-kv-node/src/index.ts
@@ -1,29 +1,30 @@
 import { Contract, Context } from 'fabric-contract-api';
 import { Asset } from './AssetInterface';
-const { nanoid } = require('nanoid');
+
 class KVContract extends Contract {
 	constructor() {
 		super('KVContract');
 	}
 
-	async instantiate() {
+	instantiate(): void {
 		console.log('Main instantiated');
 	}
 
-	async getCertificate(ctx: Context) {
-		return await ctx.clientIdentity.getID();
+	getCertificate(context: Context): string {
+		return context.clientIdentity.getID();
 	}
 
-	async createShipment(ctx: Context, uuId: string) {
+	async createShipment(context: Context, uuId: string): Promise<string> {
+		const { nanoid } = await import('nanoid');
 		const shipment: Asset = {
 			uuId,
 			uuSId: nanoid(6),
-			owner: await ctx.clientIdentity.getID(),
+			owner: context.clientIdentity.getID(),
 			pastOwners: [],
 			parentShipments: [],
 		};
 
-		await ctx.stub.putState(
+		await context.stub.putState(
 			shipment.uuId + shipment.uuSId,
 			Buffer.from(JSON.stringify(shipment)),
 		);
@@ -31,29 +32,37 @@ class KVContract extends Contract {
 		return shipment.uuId + shipment.uuSId;
 	}
 
-	async getShipment(ctx: Context, totalShimpentId: string) {
-		const shipment = await ctx.stub.getState(totalShimpentId);
-		if (!shipment || shipment.length === 0) {
+	async getShipment(
+		context: Context,
+		totalShimpentId: string,
+	): Promise<string> {
+		const shipment = await context.stub.getState(totalShimpentId);
+		if (shipment.length === 0) {
 			throw new Error(`Shipment ${totalShimpentId} does not exist`);
 		}
 		return shipment.toString();
 	}
 
 	async transferShipment(
-		ctx: Context,
+		context: Context,
 		totalShimpentId: string,
 		newOwner: string,
-	) {
+	): Promise<
+		| string
+		| {
+				response: 'ok';
+		  }
+	> {
 		let shipment;
 
 		try {
-			shipment = await ctx.stub.getState(totalShimpentId);
-		} catch (error) {
+			shipment = await context.stub.getState(totalShimpentId);
+		} catch {
 			return 'NOT_FOUND';
 		}
 		const shipmentData: Asset = JSON.parse(shipment.toString());
 
-		if (shipmentData.owner !== (await this.getCertificate(ctx))) {
+		if (shipmentData.owner !== this.getCertificate(context)) {
 			return 'NOT_ALLOWED';
 		}
 
@@ -64,7 +73,7 @@ class KVContract extends Contract {
 		shipmentData.pastOwners.push(shipmentData.owner);
 		shipmentData.owner = newOwner;
 
-		await ctx.stub.putState(
+		await context.stub.putState(
 			totalShimpentId,
 			Buffer.from(JSON.stringify(shipmentData)),
 		);
@@ -72,25 +81,24 @@ class KVContract extends Contract {
 	}
 
 	async transformShipment(
-		ctx: Context,
+		context: Context,
 		uuId: string,
-		parentShipmentIdsStr: string,
-	) {
-		const parentShipmentIds: string[] = JSON.parse(parentShipmentIdsStr);
+		parentShipmentIdsString: string,
+	): Promise<string> {
+		const parentShipmentIds: string[] = JSON.parse(parentShipmentIdsString);
 		const parentShipments: string[] = [];
 
 		for (const parentShipmentId of parentShipmentIds) {
-			let parentShipment: Asset;
 			try {
-				const shipment = await ctx.stub.getState(parentShipmentId);
+				const shipment = await context.stub.getState(parentShipmentId);
 
-				if (!shipment || shipment.length === 0) {
+				if (shipment.length === 0) {
 					throw new Error(`Shipment ${parentShipmentId} does not exist`);
 				}
 
 				const parentShipment: Asset = JSON.parse(shipment.toString());
 
-				if (parentShipment.owner !== (await this.getCertificate(ctx))) {
+				if (parentShipment.owner !== this.getCertificate(context)) {
 					throw new Error(`Shipment: ${parentShipmentId} is not owned by you`);
 				}
 
@@ -99,64 +107,79 @@ class KVContract extends Contract {
 				throw new Error(String(error));
 			}
 		}
-
+		const { nanoid } = await import('nanoid');
 		const newshipment: Asset = {
 			uuId,
 			uuSId: nanoid(6),
-			owner: ctx.clientIdentity.getID(),
+			owner: context.clientIdentity.getID(),
 			pastOwners: [],
 			parentShipments,
 		};
 
-		await ctx.stub.putState(
+		await context.stub.putState(
 			newshipment.uuId + newshipment.uuSId,
 			Buffer.from(JSON.stringify(newshipment)),
 		);
 		return newshipment.uuId + newshipment.uuSId;
 	}
 
-	async getPath(ctx: Context, totalShimpentId: string) {
-		const shipment = await ctx.stub.getState(totalShimpentId);
-		const prevOwnerNodes: string[] = [];
+	async getPath(
+		context: Context,
+		totalShimpentId: string,
+	): Promise<{
+		nodes: string[];
+		edges: {
+			type: string;
+			from: string;
+			to: string;
+		}[];
+	}> {
+		const shipment = await context.stub.getState(totalShimpentId);
+		const previousOwnerNodes: string[] = [];
 		const edges: { type: string; from: string; to: string }[] = [];
 
-		if (!shipment || shipment.length === 0) {
+		if (shipment.length === 0) {
 			throw new Error(`Shipment ${totalShimpentId} does not exist`);
 		}
 
 		const shipmentData: Asset = JSON.parse(shipment.toString());
 
 		return await this.DagCreateRecursion(
-			ctx,
+			context,
 			shipmentData,
-			prevOwnerNodes,
+			previousOwnerNodes,
 			edges,
 		);
 	}
 
 	private async DagCreateRecursion(
-		ctx: Context,
+		context: Context,
 		shipmentData: Asset,
-		prevOwnerNodes: string[] = [],
+		previousOwnerNodes: string[] = [],
 		edges: { type: string; from: string; to: string }[],
-	) {
-		if (shipmentData.pastOwners.length != 0) {
-			prevOwnerNodes.push(
-				KVContract.GetNameFromCertificate(
-					shipmentData.pastOwners[shipmentData.pastOwners.length - 1],
-				),
+	): Promise<{
+		nodes: string[];
+		edges: {
+			type: string;
+			from: string;
+			to: string;
+		}[];
+	}> {
+		if (shipmentData.pastOwners.length > 0) {
+			previousOwnerNodes.push(
+				KVContract.GetNameFromCertificate(shipmentData.pastOwners.at(-1) ?? ''),
 			);
 			edges.push({
 				type: 'transfer',
 				from: KVContract.GetNameFromCertificate(
-					shipmentData.pastOwners[shipmentData.pastOwners.length - 1],
+					shipmentData.pastOwners.at(-1) ?? '',
 				),
 				to: KVContract.GetNameFromCertificate(shipmentData.owner),
 			});
 		}
 
 		for (let i = shipmentData.pastOwners.length - 2; i >= 0; i--) {
-			prevOwnerNodes.push(
+			previousOwnerNodes.push(
 				KVContract.GetNameFromCertificate(shipmentData.pastOwners[i]),
 			);
 			edges.push({
@@ -170,13 +193,13 @@ class KVContract extends Contract {
 			let parentShipment: Asset;
 
 			try {
-				const shipment = await ctx.stub.getState(parentShipmentId);
+				const shipment = await context.stub.getState(parentShipmentId);
 				parentShipment = await JSON.parse(shipment.toString());
 			} catch (error) {
 				throw new Error(String(error));
 			}
 
-			prevOwnerNodes.push(
+			previousOwnerNodes.push(
 				KVContract.GetNameFromCertificate(parentShipment.owner),
 			);
 
@@ -187,27 +210,24 @@ class KVContract extends Contract {
 			});
 
 			const parentDAG = await this.DagCreateRecursion(
-				ctx,
+				context,
 				parentShipment,
 				[],
 				[],
 			);
-			prevOwnerNodes.push(...parentDAG.nodes);
+			previousOwnerNodes.push(...parentDAG.nodes);
 			edges.push(...parentDAG.edges);
 		}
 
-		return { nodes: prevOwnerNodes, edges };
+		return { nodes: previousOwnerNodes, edges };
 	}
 
-	private static GetNameFromCertificate(Certificate: string) {
-		if (!Certificate) {
+	private static GetNameFromCertificate(certificate: string): string {
+		if (!certificate) {
 			return 'Certificate';
 		}
-		const index = Certificate.indexOf('/CN=');
-		const name = Certificate.substring(
-			index + 4,
-			Certificate.indexOf(':', index),
-		);
+		const index = certificate.indexOf('/CN=');
+		const name = certificate.slice(index + 4, certificate.indexOf(':', index));
 		return name;
 	}
 }
