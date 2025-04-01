51a88edaa943d49459ad09e7614bb81f2b45629e
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-04-01 11:06:24 -0700

    switch to better code library

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -72,7 +72,7 @@ const authGuard: Handle = async ({ event, resolve }) => {
 	event.locals.user = user;
 
 	if (!event.locals.session && event.url.pathname.startsWith('/private')) {
-		redirect(303, '/auth');
+		redirect(303, '/login');
 	}
 
 	if (event.locals.session && event.url.pathname === '/auth') {
