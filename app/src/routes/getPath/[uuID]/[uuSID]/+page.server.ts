a1efde718f8e484bb3d36ac53ca5bce6642610ca
p-karunya <karunya.penumalla@gmail.com>
2025-04-01 11:07:58 -0700

    Formatted code

--- app/src/routes/getPath/[uuID]/[uuSID]/+page.server.ts
+++ app/src/routes/getPath/[uuID]/[uuSID]/+page.server.ts
@@ -3,35 +3,34 @@ import { ADMIN_ID, ADMIN_PASSWORD } from '$env/static/private';
 import type { PageServerLoad } from '../../$types';
 
 export const load: PageServerLoad = async ({ params }) => {
+	const { token: adminToken } = (await (
+		await fetch(`http://${PUBLIC_BLOCKCHAIN_URL}/user/enroll`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				id: ADMIN_ID,
+				secret: ADMIN_PASSWORD,
+			}),
+		})
+	).json()) as { token: string };
 
-    const { token: adminToken } = (await (
-        await fetch(`http://${PUBLIC_BLOCKCHAIN_URL}/user/enroll`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                id: ADMIN_ID,
-                secret: ADMIN_PASSWORD,
-            }),
-        })
-    ).json()) as { token: string };
+	const totalShimpmentID = String(params.uuID) + '/' + String(params.uuSID);
 
-    const totalShimpmentID = String(params.uuID) + "/" + String(params.uuSID)
+	const getPathRequest = await fetch(`http://${PUBLIC_BLOCKCHAIN_URL}/invoke/supplychain/main`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${adminToken}`,
+		},
+		body: JSON.stringify({
+			method: 'KVContract:getPath',
+			args: [totalShimpmentID],
+		}),
+	});
 
-    const getPathRequest = await fetch(`http://${PUBLIC_BLOCKCHAIN_URL}/invoke/supplychain/main`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${adminToken}`,
-        },
-        body: JSON.stringify({
-            "method": "KVContract:getPath",
-            "args": [totalShimpmentID],
-        })
-    });
+	const getPathResponse = await getPathRequest.json();
 
-    const getPathResponse = await getPathRequest.json();
-    
-    return {path: getPathResponse};
+	return { path: getPathResponse };
 };
--- app/src/routes/getPath/[uuID]/[uuSID]/+page.svelte
+++ app/src/routes/getPath/[uuID]/[uuSID]/+page.svelte
@@ -2,9 +2,8 @@
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
-    
 </script>
 
 <p>
-    {JSON.stringify(data.path)}
+	{JSON.stringify(data.path)}
 </p>
