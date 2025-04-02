0d37ac30e5f708a12aa66d967f44e4924ab1ecf5
p-karunya <karunya.penumalla@gmail.com>
2025-04-02 06:14:22 -0700

    comestic changes

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
 	import { buttonVariants } from '$lib/components/ui/button';
 	import * as Dialog from '$lib/components/ui/dialog';
+	import { onMount } from 'svelte';
 	import BarcodeScanner from './BarcodeScanner.svelte';
+	import TypingAnimation from './TypingAnimation.svelte';
 
 	let open = $state(false);
+
+	const taglines: string[] = [
+		'Trace your food',
+		'Know your food source',
+		'Sustainable eating made easy',
+		'From farm to table, we track it all',
+	];
+
+	// Randomly select a tagline from the array
+	function getRandomTagline() {
+		const randomIndex = Math.floor(Math.random() * taglines.length);
+		return taglines[randomIndex];
+	}
+
+	let tagline = $state(getRandomTagline());
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			tagline = getRandomTagline();
+		}, 4000);
+	});
 </script>
 
 <svelte:head>
@@ -12,8 +35,10 @@
 
 <div class="container flex flex-col items-center mt-8 mx-auto">
 	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
-		Trace your item
+		Foodchain
 	</h1>
+
+	<TypingAnimation outerClass="p-2 m-2">{tagline}</TypingAnimation>
 
 	<Dialog.Root bind:open>
 		<Dialog.Trigger
---
+++ app/src/routes/TypingAnimation.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
+
+	let {
+		textClass = 'text-background-900 dark:text-background-100 font-mono text-lg font-bold',
+		height = '100%',
+		children,
+		outerClass = '',
+	}: {
+		textClass?: string;
+		height?: string;
+		children?: Snippet;
+		outerClass?: string;
+	} = $props();
+</script>
+
+<div
+	class={cn('flex items-center justify-center width-fit border-black', outerClass)}
+	style={`height: ${height};`}
+>
+	<p class={cn('inline-block overflow-hidden whitespace-nowrap animate-typeAndDelete', textClass)}>
+		{@render children?.()}
+	</p>
+</div>
+
+<style>
+	@keyframes typeAndDelete {
+		0%,
+		10% {
+			width: 0;
+		}
+		45%,
+		55% {
+			width: 100%;
+		}
+		90%,
+		100% {
+			width: 0;
+		}
+	}
+
+	.animate-typeAndDelete {
+		animation: typeAndDelete 4s linear infinite;
+	}
+</style>
